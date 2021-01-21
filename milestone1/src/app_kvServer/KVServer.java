b9c494b0dbcd8f1bbf33c7590f4050ab3a19673b
cwangCharlie <wang.charlie04@gmail.com>
2021-01-21 15:12:03 -0500

    cleaning up master

--- milestone1/src/app_kvServer/KVServer.java
+++ milestone1/src/app_kvServer/KVServer.java
@@ -1,29 +1,6 @@
 package app_kvServer;
 
-import shared.communication.KVCommunication;
-import logger.LogSetup;
-
-import java.util.ArrayList;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.UnknownHostException;
-import java.net.BindException;
-import java.net.Socket;
-import java.io.IOException;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
 public class KVServer implements IKVServer {
-	
-	private static Logger logger = Logger.getRootLogger();
-	private ServerSocket serverSocket;
-	private int port;
-	private int cacheSize;
-	private String strategy;
-	private boolean running;
-	private ArrayList<Thread> clientThreads;
-
 	/**
 	 * Start KV Server at given port
 	 * @param port given port for storage server to operate
@@ -35,142 +12,78 @@ public class KVServer implements IKVServer {
 	 *           and "LFU".
 	 */
 	public KVServer(int port, int cacheSize, String strategy) {
-		this.serverSocket = null;
-		this.port = port;
-		this.cacheSize = cacheSize;
-		this.clientThreads = new ArrayList<Thread>();
+		// TODO Auto-generated method stub
 	}
 	
 	@Override
 	public int getPort(){
-		return this.port;
+		// TODO Auto-generated method stub
+		return -1;
 	}
 
 	@Override
     public String getHostname(){
-		String hostname = "";
-		try {
-			hostname = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			logger.error("The IP address of server host cannot be resolved. \n", e);
-		}
-		return hostname;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
     public CacheStrategy getCacheStrategy(){
-		switch(this.strategy){
-			case "None":
-				return IKVServer.CacheStrategy.None;
-			case "LRU":
-				return IKVServer.CacheStrategy.LRU;
-			case "LFU":
-				return IKVServer.CacheStrategy.LFU;
-			case "FIFO":
-				return IKVServer.CacheStrategy.FIFO;
-			default:
-				logger.error("Undefined use of IKVServer.CacheStrategy, setting to None.");
-				return IKVServer.CacheStrategy.None;	
-		}
+		// TODO Auto-generated method stub
+		return IKVServer.CacheStrategy.None;
 	}
 
 	@Override
     public int getCacheSize(){
-		return this.cacheSize;
+		// TODO Auto-generated method stub
+		return -1;
 	}
 
 	@Override
     public boolean inStorage(String key){
-		////TODO seach for key and return boolean
+		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
     public boolean inCache(String key){
-		////TODO seach for key and return boolean
+		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
     public String getKV(String key) throws Exception{
-		////TODO search for key and return its value
+		// TODO Auto-generated method stub
 		return "";
 	}
 
 	@Override
     public void putKV(String key, String value) throws Exception{
-		////TODO store key-value pair
+		// TODO Auto-generated method stub
 	}
 
 	@Override
     public void clearCache(){
-		////TODO clear cache
+		// TODO Auto-generated method stub
 	}
 
 	@Override
     public void clearStorage(){
-		////TODO clear storage
+		// TODO Auto-generated method stub
 	}
 
 	@Override
     public void run(){
-		logger.info("Initialize server ...");
-		try {
-			serverSocket = new ServerSocket(port);
-			logger.info("Server listening on port: " + serverSocket.getLocalPort());    
-			running = true;
-		}
-		catch (IOException e) {
-			logger.error("Error! Cannot open server socket. \n", e);
-			if (e instanceof BindException){
-             	logger.error("Port " + port + " is already bound! \n");
-            }
-			running = false;
-        }
-        
-        if (serverSocket != null) {
-	        while (running){
-	            try {
-					Socket client = serverSocket.accept();
-					KVCommunication communicator = new KVCommunication(client, this);
-	                Thread clientThread = new Thread(communicator);
-	                clientThread.start();
-	                clientThreads.add(clientThread);              
-					logger.info("Connected to " + client.getInetAddress().getHostName() +  
-							" on port " + client.getPort());
-				} 
-				catch (IOException e) {
-					logger.error("Error! Unable to establish connection. \n", e);
-	            }
-	        }
-        }
-        logger.info("Server stopped.");
+		// TODO Auto-generated method stub
 	}
 
 	@Override
     public void kill(){
-		////TODO save to storage
-		////TODO force shut down server
-		running = false;
-		try {
-			serverSocket.close();
-		} catch (IOException e) {
-			logger.error("Error! Unable to close socket on port: " + port, e);
-		}
+		// TODO Auto-generated method stub
 	}
 
 	@Override
     public void close(){
-		////TODO perform any action
-		////TODO gracefully shut down server
-		running = false;
-		try {
-			for (int i = 0; i < clientThreads.size(); i++){
-                clientThreads.get(i).interrupt();
-            }
-			serverSocket.close();
-		} catch (IOException e) {
-			logger.error("Error! Unable to close socket on port: " + port, e);
-		}
+		// TODO Auto-generated method stub
 	}
 }
